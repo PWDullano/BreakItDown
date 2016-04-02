c7bdd2f515868ec310c8437b7903841ef74ab04f
Patrick Dullano <PWDullano@gmail.com>
2016-04-01 23:31:04 -0600

    initial angular routing and express setup

---
+++ ang_app/app/js/app.js
@@ -0,0 +1,31 @@
+var app = angular.module("myApp", ['ngRoute', 'ngResource']);
+
+
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/splash.html',
+        controller: 'MainController'
+      })
+      .when('/dashboard', {
+        templateUrl: 'partials/dashboard.html',
+        controller: 'MainController'
+      })
+      .when('/sessions', {
+        templateUrl: 'partials/sessions.html',
+        controller: 'MainController'
+      })
+      .when('/breakdown', {
+        templateUrl: 'partials/breakdown.html',
+        controller: 'MainController'
+      })
+      .when('/stripes', {
+        templateUrl: 'partials/stripes.html',
+        controller: 'MainController'
+      })
+      .when('/faq', {
+        templateUrl: 'partials/faq.html',
+        controller: 'MainController'
+      })
+    $locationProvider.html5Mode(true);
+});
