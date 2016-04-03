4355d0bd8f72485d4cbc3e35225474fde3f2925e
Patrick Dullano <PWDullano@gmail.com>
2016-04-03 16:59:16 -0600

    backend deployed to heroku

--- ang_app/app/js/services.js
+++ ang_app/app/js/services.js
@@ -2,15 +2,18 @@ app.service('breakService', function($http){
   var breakService = {};
 
   breakService.getMoves = function(){
-    return $http.get('http://localhost:3000/moves')
+    // return $http.get('http://localhost:3000/moves')
+    return $http.get('https://breakitdown.herokuapp.com/moves')
   }
 
   breakService.getUser = function(){
-    return $http.get('http://localhost:3000/users')
+    // return $http.get('http://localhost:3000/users')
+    return $http.get('https://breakitdown.herokuapp.com/users')
   }
 
   breakService.getStripes = function(){
-    return $http.get('http://localhost:3000/stripes')
+    // return $http.get('http://localhost:3000/stripes')
+    return $http.get('https://breakitdown.herokuapp.com/stripes')
   }
 
   return breakService;
