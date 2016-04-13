e398e4d4173aa428338f406c4e5e7174dbaf326b
Patrick Dullano <PWDullano@gmail.com>
2016-04-13 17:52:48 -0600

    fixed posting issue

--- seperation/seeds/session_seed.js
+++ seperation/seeds/session_seed.js
@@ -5,12 +5,12 @@ exports.seed = function(knex, Promise) {
 
     // Inserts seed entries
     knex('sessions').insert({
-      id: 1,
+      id: 30,
       date: '4/12/2016',
       focus: "Windmill , 2000 , Baby Freeze , Freezes, Flares, Airflares",
       user_id: 1}),
     knex('sessions').insert({
-      id: 2,
+      id: 31,
       date: '4/14/2016',
       focus: "Top Rock, Drops, Six-Step, Baby Swipes, Swipes , Windmill, Freezes",
       user_id: 1})
